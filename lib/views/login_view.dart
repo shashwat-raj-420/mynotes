06ac40a05cdf3c6f9dbdd804443aef3558940ddd
Shashwat Raj <sh10ashwat@gmail.com>
2025-06-08 20:34:14 +0530

    step-24

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,7 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -61,23 +62,24 @@ class _LoginViewState extends State<LoginView> {
               hintStyle: TextStyle(color: Colors.grey),
             ),
           ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
-                context.read<AuthBloc>().add(AuthEventLogIn(email, password));
-              } on InvalidCredentialAuthException {
-                if (context.mounted) {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is InvalidCredentialAuthException) {
                   await showErrorDialog(context, 'Invalid Credential');
-                }
-              } on GenericAuthException {
-                if (context.mounted) {
-                  await showErrorDialog(context, 'Authentication Error');
+                } else if (state.exception is GenericAuthException) {
+                  await showErrorDialog(context, 'Authentication error');
                 }
               }
             },
-            child: const Text('Login', style: TextStyle(color: Colors.blue)),
+            child: TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                context.read<AuthBloc>().add(AuthEventLogIn(email, password));
+              },
+              child: const Text('Login', style: TextStyle(color: Colors.blue)),
+            ),
           ),
           TextButton(
             onPressed: () {
